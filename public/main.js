b329b2c1222f4d2c8f0bd46800fabc410a4e5af4
Justin Swiger <justin.swiger@me.com>
2019-10-07 20:20:18 -0400

    removed wrong js

--- public/main.js
+++ public/main.js
@@ -2,10 +2,6 @@ const main = () => {
 	if (document.querySelector('h1.hello-world')) {
 		document.querySelector('h1.hello-world').textContent = 'Hello, World!';
 	}
-	if (document.querySelector('h2')) {
-		document.querySelector('h2').style.color = 'blue';
-	}
-	document.querySelector('body').style.backgroundColor = '#666';
 };
 
 document.addEventListener('DOMContentLoaded', main);
